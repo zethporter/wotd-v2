65efe6086f554c10904b366ff7ee8cab08e16256
zeth <zethwporter@gmail.com>
2025-12-22 15:31:18 -0700

    Completely removed better auth stuff

--- src/lib/auth/auth-client.ts
+++
@@ -1,8 +0,0 @@
-// import { createAuthClient } from 'better-auth/react'
-// import { adminClient, organizationClient } from 'better-auth/client/plugins'
-
-// export const { useSession, signIn, signOut, signUp, getSession, admin } =
-//   createAuthClient({
-//     redirectTo: '/manage',
-//     plugins: [adminClient(), organizationClient()],
-//   })
